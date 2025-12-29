61ae968f962d5f7da1fe71ea40c4175d259c789e
truongvv <vuvantruong120896@gmail.com>
2025-12-29 15:01:02 +0700

    Modify git ignore

---
+++ EMIC_LORA_PROTOCOL/HardwareDebug/src/app/EXAMPLE_PERIODIC_TX.d
@@ -0,0 +1,9 @@
+src/app/EXAMPLE_PERIODIC_TX.obj src/app/EXAMPLE_PERIODIC_TX.d: ../src/app/EXAMPLE_PERIODIC_TX.c
+src/app/EXAMPLE_PERIODIC_TX.obj src/app/EXAMPLE_PERIODIC_TX.d: ../src/app/app_periodic_tx.h
+../src/app/app_periodic_tx.h:
+src/app/EXAMPLE_PERIODIC_TX.obj src/app/EXAMPLE_PERIODIC_TX.d: D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\hal\hal_systick.h
+D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\hal\hal_systick.h:
+src/app/EXAMPLE_PERIODIC_TX.obj src/app/EXAMPLE_PERIODIC_TX.d: D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\hal\hal_uart.h
+D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\hal\hal_uart.h:
+src/app/EXAMPLE_PERIODIC_TX.obj src/app/EXAMPLE_PERIODIC_TX.d: D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\utils\log_control.h
+D:\Projects\Emic\LoraSafe\Core\EMIC_LORA_PROTOCOL\src\utils\log_control.h:
